ac306c943238c2b72a7b5832a99eca7abdd3a4a1
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-03-10 13:44:33 -0700

    Fortune button working

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,14 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
-    }
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["You're heading in the right direction", "Wow! I never thought you'd make it, but here you are", "Your mother was a hamster", "A person of words and not deeds is like a garden full of weeds. So get out there and do something", "A pleasant surprise is waiting for you", "A short pencil is usually better than a long memory any day. Are you taking notes?", "Everyday in your life is a special occasion. Seize the day", "Success is failure turned inside out. Learn from your failures and keep moving", "Technology is the art of arranging the world so we do not notice it"];
 
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    }
+    
 }
